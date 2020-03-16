510e684d9441c766de25e621d99e82371c967b91
zaid <khanzaid786786.kz@gmail.com>
2020-03-16 14:26:39 +0530

    Updates

---
+++ precache-manifest.4b3d6d0bdd4b224e3d6e881f520b3af3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0484ed300de2269dbcb0200ff5b2ba69",
+    "url": "/bewakoof_app/index.html"
+  },
+  {
+    "revision": "e939b4340adf841aed1e",
+    "url": "/bewakoof_app/static/css/2.dea561c2.chunk.css"
+  },
+  {
+    "revision": "cd441761a41b541c0895",
+    "url": "/bewakoof_app/static/css/main.e4dc6f20.chunk.css"
+  },
+  {
+    "revision": "e939b4340adf841aed1e",
+    "url": "/bewakoof_app/static/js/2.3daedc29.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/bewakoof_app/static/js/2.3daedc29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd441761a41b541c0895",
+    "url": "/bewakoof_app/static/js/main.240a64c2.chunk.js"
+  },
+  {
+    "revision": "eb97e7ec7818df87159a",
+    "url": "/bewakoof_app/static/js/runtime-main.5824d917.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/bewakoof_app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/bewakoof_app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/bewakoof_app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/bewakoof_app/static/media/slick.f97e3bbf.svg"
+  }
+]);
