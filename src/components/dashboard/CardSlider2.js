480f7ab3a9b128658b13cabdf74fc2ec6c88cf14
zaid <khanzaid786786.kz@gmail.com>
2020-03-16 15:33:09 +0530

    added

--- src/components/dashboard/CardSlider2.js
+++ src/components/dashboard/CardSlider2.js
@@ -20,7 +20,7 @@ export default class Responsive extends Component {
       <div>
 
         <br/><br/>
-        <div>
+        <div className="ui container">
         <Slider {...settings}>
             <div>
                 <img src="banner1.jpg" className="cardsliderimg2" />
